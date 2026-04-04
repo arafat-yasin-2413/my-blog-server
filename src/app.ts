578a47e70053359cffb8a236cdbfb889eecea9b7
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-04-04 09:49:41 +0600

    (24-1): sets up Better auth

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express, { Application } from "express";
 import { postRouter } from "./modules/post/post.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 // import cors from "cors";
 
 const app: Application = express();
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+// 24-1 er 11:20 er por eikhane /api/auth/*splat likhte bola hoisilo.
+// Kintu ami Better auth er docu onujayi likhechi. And So far, it is working.
+
+
 app.use(express.json());
 // app.use(cors());
 
