b6ec249ff9dad9e44ab3bd1c576ee71d9dd26e98
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-04-05 11:20:48 +0600

    (24-8, 24-9, 24-10): implements role-based-auth middleware

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -5,8 +5,14 @@ import { postServices } from "./post.service";
 const createPost = async(req:Request, res:Response) =>{
 
     try{
-        const result = await postServices.createPost(req.body);
-        console.log("Body in controller : ---------", req.body)
+        const user = req.user;
+        console.log('user in create-post-controller : ', {user});
+        if(!req.user) {
+            return res.status(401).json({
+                error: "Unauthorized access",
+            })
+        }
+        const result = await postServices.createPost(req.body, user?.id as string);
         res.status(201).json(result);
     
     }
