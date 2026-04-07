d999b278aa184cfe6274e075acf1269acdcfcd81
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-04-07 11:12:54 +0600

    (25-4): adds get post by title API

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,44 +1,43 @@
 import { Request, Response } from "express";
 import { postServices } from "./post.service";
 
-
-const getAllPost = async(req: Request,res:Response) =>{
-    try{
-        const result = await postServices.getAllPost();
+const getAllPost = async (req: Request, res: Response) => {
+    try {
+        const { search } = req.query;
+        const searchString = typeof search === "string" ? search : undefined;
+        const result = await postServices.getAllPost({ search: searchString });
         return res.status(200).json(result);
-    }
-    catch(error) {
+    } catch (error) {
         return res.status(400).json({
             error: "Getting all post operation failed",
-            details: error
-        })
+            details: error,
+        });
     }
-}
+};
 
-const createPost = async(req:Request, res:Response) =>{
-
-    try{
+const createPost = async (req: Request, res: Response) => {
+    try {
         const user = req.user;
-        console.log('user in create-post-controller : ', {user});
-        if(!req.user) {
+        console.log("user in create-post-controller : ", { user });
+        if (!req.user) {
             return res.status(401).json({
                 error: "Unauthorized access",
-            })
+            });
         }
-        const result = await postServices.createPost(req.body, user?.id as string);
+        const result = await postServices.createPost(
+            req.body,
+            user?.id as string,
+        );
         res.status(201).json(result);
-    
-    }
-    catch(error) {
+    } catch (error) {
         res.status(400).json({
             error: "Post Creation Failed",
-            details: error
-        })
+            details: error,
+        });
     }
-}
-
+};
 
 export const postController = {
     createPost,
     getAllPost,
-}
+};
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,26 +1,32 @@
-
 import { Post } from "../../generated/prisma/client";
 import { prisma } from "../../lib/prisma";
 
-
-const getAllPost = async() => {
-    const allPost = await prisma.post.findMany();
+const getAllPost = async (payload: { search: string | undefined }) => {
+    const allPost = await prisma.post.findMany({
+        where: {
+            title: {
+                contains: payload.search as string,
+                mode: "insensitive",
+            }
+        }
+    });
     return allPost;
-}
+};
 
-const createPost = async (data: Omit<Post, 'id' | 'createdAt' | 'updatedAt' | 'authorId'>, userId: string) =>{
+const createPost = async (
+    data: Omit<Post, "id" | "createdAt" | "updatedAt" | "authorId">,
+    userId: string,
+) => {
     const result = await prisma.post.create({
         data: {
             ...data,
-            authorId: userId
-        }
-    })
+            authorId: userId,
+        },
+    });
     return result;
-
-}
-
+};
 
 export const postServices = {
     createPost,
-    getAllPost
-}
+    getAllPost,
+};
