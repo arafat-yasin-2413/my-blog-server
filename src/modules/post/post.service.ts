ed9ac787c84c5060dc384809789ea54e66d177a7
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-04-08 18:25:43 +0600

    (26-7): applies refactoring in post.service.ts file

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -22,8 +22,8 @@ const getAllPost = async ({
     page: number;
     limit: number;
     skip: number;
-    sortBy: string | undefined;
-    sortOrder: string | undefined;
+    sortBy: string;
+    sortOrder: string;
 }) => {
     const andConditions: PostWhereInput[] = [];
 
@@ -83,8 +83,7 @@ const getAllPost = async ({
         where: {
             AND: andConditions,
         },
-        orderBy:
-            sortBy && sortOrder ? { [sortBy]: sortOrder } : { createdAt: "desc" },
+        orderBy: { [sortBy]: sortOrder },
     });
     return allPost;
 };
