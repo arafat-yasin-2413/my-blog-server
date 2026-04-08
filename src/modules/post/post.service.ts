04ffb55b1fb9fb53572de46c04f43e50a80b8c83
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-04-09 01:00:33 +0600

    (26-10): refactoring that adds transaction

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -88,42 +88,44 @@ const getAllPost = async ({
 
     const total = await prisma.post.count({
         where: {
-            AND: andConditions
-        }
-    })
+            AND: andConditions,
+        },
+    });
 
     return {
         data: allPost,
-        pagination:{
+        pagination: {
             total,
             page,
             limit,
-            totalPages: Math.ceil(total/limit)
-        }
+            totalPages: Math.ceil(total / limit),
+        },
     };
 };
 
-const getPostById = async(id:string) =>{
+const getPostById = async (id: string) => {
+    return await prisma.$transaction(async (tx) => {
+        await tx.post.update({
+            where: {
+                id: id,
+            },
+            data: {
+                views: {
+                    increment: 1,
+                },
+            },
+        });
 
-    const updatedViewCount = await prisma.post.update({
-        where:{
-            id: id
-        },
-        data:{
-            views: {
-                increment: 1
-            }
-        }
-    })
+        const postData = await tx.post.findUnique({
+            where: {
+                id: id,
+            },
+        });
+        return postData;
+    });
 
-    const result = await prisma.post.findUnique({
-        where: {
-            id: id
-        }
-    })
-
-    return result;
-}
+    // return result;
+};
 
 const createPost = async (
     data: Omit<Post, "id" | "createdAt" | "updatedAt" | "authorId">,
