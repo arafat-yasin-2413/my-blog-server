f5bb79125be351cfe8b66ccc82533fd565d2e63d
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-04-09 00:29:23 +0600

    (26-8): retrives post data with pagination meta data

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -85,7 +85,22 @@ const getAllPost = async ({
         },
         orderBy: { [sortBy]: sortOrder },
     });
-    return allPost;
+
+    const total = await prisma.post.count({
+        where: {
+            AND: andConditions
+        }
+    })
+
+    return {
+        data: allPost,
+        pagination:{
+            total,
+            page,
+            limit,
+            totalPages: Math.ceil(total/limit)
+        }
+    };
 };
 
 const createPost = async (
