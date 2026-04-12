d1040480f39a39f82a55436486ce3644e7ac468d
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-04-12 22:48:24 +0600

    (29-2): implements prisma client validation error

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,11 +1,23 @@
 import { NextFunction, Request, Response } from "express"
+import { Prisma } from "../generated/prisma/client";
 
 
 export default function errorHandler(err:any, req:Request, res:Response, next:NextFunction) {
-    res.status(500)
+    
+    let statusCode = 500;
+    let errorMessage = 'Internal Server Error';
+    let errorDetails = err;
+
+    // PrismaClientValidationError
+    if(err instanceof Prisma.PrismaClientValidationError){
+        statusCode = 400;
+        errorMessage = "Incorrect field type or missing fields!"
+    }
+    
+    res.status(statusCode)
     res.json({
-        message: "Error from global error handler!",
-        error: err
+        message: errorMessage,
+        error: errorDetails
     })
 }
 
